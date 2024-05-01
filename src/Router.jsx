3eaabc2513a9fbae94da2a053c587822c3591d7a
CoPinch <Johnkennethmacabitas11@gmail.com>
2024-05-01 15:34:52 +0800

    Dashboard

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LandingPage from "./views/LandingPage";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Dashboard from "./views/Dashboard";
+
 
 const Router = () => (
     <>
@@ -10,6 +12,11 @@ const Router = () => (
             <Route path="/Home" element={<LandingPage/>} />
             <Route path="/LoginPage" element={<Login/>} />
             <Route path="/SignUpPage" element={<Register/>} />
+            
+            <Route path="/Dashboard" element={<Dashboard/>}> 
+            
+            </Route> 
+            
         </Routes>
     </>
 );
