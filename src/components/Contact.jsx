9d73d05dddfbfdf0004efb229a2cf9dd6d01875b
Paolo Banaglorioso <93817250+chippee08@users.noreply.github.com>
2024-05-05 14:52:30 +0800

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,9 +18,9 @@ import logo from "../assets/logo.png"
 
       <div className="space-y-4 w-full flex justify-center">
         <form action="#" className="flex flex-col p-6 space-y-4 xsm:w-screen sm:w-1/3">
-          <input type="text" placeholder="Name" className="text-"/>
-          <input type="text" placeholder="Email" className="" />
-          <textarea name="" id="" cols="30" rows="5" placeholder="Message"></textarea>
+          <input type="text" placeholder="Name" className="text-" required/>
+          <input type="text" placeholder="Email" className="" required/>
+          <textarea name="" id="" cols="30" rows="5" placeholder="Message" required></textarea>
           <button className="bg-blue-700 text-xl p-2 text-orange-100 hover:bg-gray-700">Send</button>
         </form>
       </div>
