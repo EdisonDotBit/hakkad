0e3d065a8bb13b20cf650f805085f1eb25cee36b
Kczar <kenarnold123213@gmail.com>
2024-05-21 01:46:50 +0800

    Revert "A"
    This reverts commit bb3526c9e82ad7274468414a28afc9816bec6816.

--- src/Router.jsx
+++ src/Router.jsx
@@ -13,43 +13,32 @@ import Lesson1 from "./components/Lesson1.jsx";
 import Lesson2 from "./components/Lesson2.jsx";
 import Lesson3 from "./components/Lesson3.jsx";
 import EditProfile from "./components/EditProfile.jsx";
-import { AuthProvider } from "./Hooks/useAuth.jsx";
-import { ProtectedRoute } from "./ProtectedRoute.jsx";
 
 const Router = () => (
   <>
-    <AuthProvider>
-      <Routes>
-        <Route path="*" element={<Navigate to="/Home" />} />
-        <Route path="/Home" element={<LandingPage />} />
-        <Route path="/LoginPage" element={<Login />} />
-        <Route path="/SignUpPage" element={<Register />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Profile" element={<EditProfile />} />
+    <Routes>
+      <Route path="*" element={<Navigate to="/Home" />} />
+      <Route path="/Home" element={<LandingPage />} />
+      <Route path="/LoginPage" element={<Login />} />
+      <Route path="/SignUpPage" element={<Register />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/Profile" element={<EditProfile />} />
 
-        {/* Lessons */}
-        <Route path="/Lesson1" element={<Lesson1 />} />
-        <Route path="/Lesson2" element={<Lesson2 />} />
-        <Route path="/Lesson3" element={<Lesson3 />} />
+      {/* Lessons */}
+      <Route path="/Lesson1" element={<Lesson1 />} />
+      <Route path="/Lesson2" element={<Lesson2 />} />
+      <Route path="/Lesson3" element={<Lesson3 />} />
 
-        <Route path="/Student" element={<StudentLayout />}>
-          <Route path="" element={<DashboardContent />} />
-          <Route
-            path="Dashboard"
-            element={
-              <ProtectedRoute>
-                <DashboardContent />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="Quiz" element={<Quiz />} />
-          <Route path="Leaderboard" element={<Leaderboard />} />
+      <Route path="/Student" element={<StudentLayout />}>
+        <Route path="" element={<Navigate to="/Student/Dashboard" />} />
+        <Route path="Dashboard" element={<DashboardContent />} />
+        <Route path="Quiz" element={<Quiz />} />
+        <Route path="Leaderboard" element={<Leaderboard />} />
 
-          <Route path="Quiz1" element={<Quiz1 />} />
-          <Route path="Quiz2" element={<Quiz2 />} />
-        </Route>
-      </Routes>
-    </AuthProvider>
+        <Route path="Quiz1" element={<Quiz1 />} />
+        <Route path="Quiz2" element={<Quiz2 />} />
+      </Route>
+    </Routes>
   </>
 );
 
